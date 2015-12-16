29d0c45f1d1697d24086eeb705f2b92b9c7d0c51
Max <contact@mstoiber.com>
2015-12-16 13:28:08 +0100

    Fix typo

--- js/components/pages/HomePage.react.js
+++ js/components/pages/HomePage.react.js
@@ -27,7 +27,7 @@ class HomePage extends Component {
 							<h1>Welcome to Login Flow!</h1>
 						)}
 						<p>This application demonstrates what a React.js based register/login workflow might look like on the Frontend. I used <a href="https://github.com/mxstbr/react-boilerplate">react-boilerplate</a> as a starting point — the app thus uses Redux, PostCSS, react-router, ServiceWorker, AppCache, bcrypt and lots more.</p>
-						<p>he default username is <code>AzureDiamond</code> and the default password is <code>hunter2</code>, but feel free to register new users! The registered users are saved to localStorage, so they'll persist across page reloads.</p>
+						<p>The default username is <code>AzureDiamond</code> and the default password is <code>hunter2</code>, but feel free to register new users! The registered users are saved to localStorage, so they'll persist across page reloads.</p>
 						{loggedIn ? null : (
 							<div>
 								<Link to="/login" className="btn btn--login">Log In</Link>
