f2d2856fc8816be6937a79bc02a3ecb64247888d
Max <contact@mstoiber.com>
2015-12-16 15:49:03 +0100

    Add .htaccess to build folder

--- js/app.js
+++ js/app.js
@@ -7,10 +7,11 @@
  *
  */
 
-// Load the ServiceWorker, the Cache polyfill, the manifest.json file
+// Load the ServiceWorker, the Cache polyfill, the manifest.json file and the .htaccess file
 import 'file?name=[name].[ext]!../serviceworker.js';
 import 'file?name=[name].[ext]!../serviceworker-cache-polyfill.js';
 import 'file?name=[name].[ext]!../manifest.json';
+import 'file?name=[name].[ext]!../.htaccess';
 
 // Check for ServiceWorker support before trying to install it
 if ('serviceWorker' in navigator) {
