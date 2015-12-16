25a61aa20b8c19b373405823a16c249105cec077
Max <contact@mstoiber.com>
2015-12-16 12:19:57 +0100

    Explain history.js module

--- js/utils/history.js
+++ js/utils/history.js
@@ -1,2 +1,6 @@
+/**
+ * This is a module so we can use it outside of components, e.g. in actions (see actions/AppActions.js)
+ */
+
 import createHistory from 'history/lib/createBrowserHistory';
 export default createHistory();
