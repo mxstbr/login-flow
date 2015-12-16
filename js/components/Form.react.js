b26cfabfdf67b6ca09047ec2466b41f7ce7458bd
Max <contact@mstoiber.com>
2015-12-16 13:05:29 +0100

    Disable autocorrect etc for username input field

--- js/components/Form.react.js
+++ js/components/Form.react.js
@@ -24,7 +24,7 @@ class LoginForm extends Component {
           <p className="form__error form__error--field-missing">Please fill out the entire form.</p>
         </div>
         <div className="form__field-wrapper">
-          <input className="form__field-input" type="text" id="username" value={this.props.data.username} placeholder="frank.underwood" onChange={this._changeUsername.bind(this)} />
+          <input className="form__field-input" type="text" id="username" value={this.props.data.username} placeholder="frank.underwood" onChange={this._changeUsername.bind(this)} autoCorrect="off" autoCapitalize="off" spellCheck="false" />
           <label className="form__field-label" htmlFor="username">Username</label>
         </div>
         <div className="form__field-wrapper">
