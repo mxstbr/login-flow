ae29b8ba100bc85d7fbfd42c5f547d40a87da245
Andreas Reiterer <reiterer.a@gmail.com>
2017-02-06 19:32:56 +0100

    Fixed white spaces

--- js/actions/AppActions.js
+++ js/actions/AppActions.js
@@ -36,70 +36,70 @@ import { browserHistory } from 'react-router';
  * @param  {string} password The password of the user to be logged in
  */
 export function login(username, password) {
-	return (dispatch) => {
-		// Show the loading indicator, hide the last error
-		dispatch(sendingRequest(true));
-		// If no username or password was specified, throw a field-missing error
-		if (anyElementsEmpty({ username, password })) {
-			dispatch(setErrorMessage(errorMessages.FIELD_MISSING));
-			dispatch(sendingRequest(false));
-			return;
-		}
-		// Generate salt for password encryption
-		const salt = genSalt(username);
-		// Encrypt password
-		bcrypt.hash(password, salt, (err, hash) => {
-			// Something wrong while hashing
-			if (err) {
-				dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
-				return;
-			}
-			// Use auth.js to fake a request
-			auth.login(username, hash, (success, err) => {
-				// When the request is finished, hide the loading indicator
-				dispatch(sendingRequest(false));
-				dispatch(setAuthState(success));
-				if (success === true) {
-					// If the login worked, forward the user to the dashboard and clear the form
-					forwardTo('/dashboard');
-					dispatch(changeForm({
-						username: "",
-						password: ""
-					}));
-				} else {
-					switch (err.type) {
-						case 'user-doesnt-exist':
-							dispatch(setErrorMessage(errorMessages.USER_NOT_FOUND));
-							return;
-						case 'password-wrong':
-							dispatch(setErrorMessage(errorMessages.WRONG_PASSWORD));
-							return;
-						default:
-							dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
-							return;
-					}
-				}
-			});
-		});
-	}
+  return (dispatch) => {
+    // Show the loading indicator, hide the last error
+    dispatch(sendingRequest(true));
+    // If no username or password was specified, throw a field-missing error
+    if (anyElementsEmpty({ username, password })) {
+      dispatch(setErrorMessage(errorMessages.FIELD_MISSING));
+      dispatch(sendingRequest(false));
+      return;
+    }
+    // Generate salt for password encryption
+    const salt = genSalt(username);
+    // Encrypt password
+    bcrypt.hash(password, salt, (err, hash) => {
+      // Something wrong while hashing
+      if (err) {
+        dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
+        return;
+      }
+      // Use auth.js to fake a request
+      auth.login(username, hash, (success, err) => {
+        // When the request is finished, hide the loading indicator
+        dispatch(sendingRequest(false));
+        dispatch(setAuthState(success));
+        if (success === true) {
+          // If the login worked, forward the user to the dashboard and clear the form
+          forwardTo('/dashboard');
+          dispatch(changeForm({
+            username: "",
+            password: ""
+          }));
+        } else {
+          switch (err.type) {
+            case 'user-doesnt-exist':
+              dispatch(setErrorMessage(errorMessages.USER_NOT_FOUND));
+              return;
+            case 'password-wrong':
+              dispatch(setErrorMessage(errorMessages.WRONG_PASSWORD));
+              return;
+            default:
+              dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
+              return;
+          }
+        }
+      });
+    });
+  }
 }
 
 /**
  * Logs the current user out
  */
 export function logout() {
-	return (dispatch) => {
-		dispatch(sendingRequest(true));
-		auth.logout((success, err) => {
-			if (success === true) {
-				dispatch(sendingRequest(false));
-				dispatch(setAuthState(false));
-				browserHistory.replace(null, '/');
-			} else {
-				dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
-			}
-		});
-	}
+  return (dispatch) => {
+    dispatch(sendingRequest(true));
+    auth.logout((success, err) => {
+      if (success === true) {
+        dispatch(sendingRequest(false))
+        dispatch(setAuthState(false));
+        browserHistory.replace(null, '/');
+      } else {
+        dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
+      }
+    });
+  }
 }
 
 /**
@@ -108,49 +108,49 @@ export function logout() {
  * @param  {string} password The password of the new user
  */
 export function register(username, password) {
-	return (dispatch) => {
-		// Show the loading indicator, hide the last error
-		dispatch(sendingRequest(true));
-		// If no username or password was specified, throw a field-missing error
-		if (anyElementsEmpty({ username, password })) {
-			dispatch(setErrorMessage(errorMessages.FIELD_MISSING));
-			dispatch(sendingRequest(false));
-			return;
-		}
-		// Generate salt for password encryption
-		const salt = genSalt(username);
-		// Encrypt password
-		bcrypt.hash(password, salt, (err, hash) => {
-			// Something wrong while hashing
-			if (err) {
-				dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
-				return;
-			}
-			// Use auth.js to fake a request
-			auth.register(username, hash, (success, err) => {
-				// When the request is finished, hide the loading indicator
-				dispatch(sendingRequest(false));
-				dispatch(setAuthState(success));
-				if (success) {
-					// If the register worked, forward the user to the homepage and clear the form
-					forwardTo('/dashboard');
-					dispatch(changeForm({
-						username: "",
-						password: ""
-					}));
-				} else {
-					switch (err.type) {
-						case 'username-exists':
-							dispatch(setErrorMessage(errorMessages.USERNAME_TAKEN));
-							return;
-						default:
-							dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
-							return;
-					}
-				}
-			});
-		});
-	}
+  return (dispatch) => {
+    // Show the loading indicator, hide the last error
+    dispatch(sendingRequest(true));
+    // If no username or password was specified, throw a field-missing error
+    if (anyElementsEmpty({ username, password })) {
+      dispatch(setErrorMessage(errorMessages.FIELD_MISSING));
+      dispatch(sendingRequest(false));
+      return;
+    }
+    // Generate salt for password encryption
+    const salt = genSalt(username);
+    // Encrypt password
+    bcrypt.hash(password, salt, (err, hash) => {
+      // Something wrong while hashing
+      if (err) {
+        dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
+        return;
+      }
+      // Use auth.js to fake a request
+      auth.register(username, hash, (success, err) => {
+        // When the request is finished, hide the loading indicator
+        dispatch(sendingRequest(false));
+        dispatch(setAuthState(success));
+        if (success) {
+          // If the register worked, forward the user to the homepage and clear the form
+          forwardTo('/dashboard');
+          dispatch(changeForm({
+            username: "",
+            password: ""
+          }));
+        } else {
+          switch (err.type) {
+            case 'username-exists':
+              dispatch(setErrorMessage(errorMessages.USERNAME_TAKEN));
+              return;
+            default:
+              dispatch(setErrorMessage(errorMessages.GENERAL_ERROR));
+              return;
+          }
+        }
+      });
+    });
+  }
 }
 
 /**
@@ -158,7 +158,7 @@ export function register(username, password) {
  * @param {boolean} newState True means a user is logged in, false means no user is logged in
  */
 export function setAuthState(newState) {
-	return { type: SET_AUTH, newState };
+  return { type: SET_AUTH, newState };
 }
 
 /**
@@ -169,7 +169,7 @@ export function setAuthState(newState) {
  * @return {object}                   Formatted action for the reducer to handle
  */
 export function changeForm(newState) {
-	return { type: CHANGE_FORM, newState };
+  return { type: CHANGE_FORM, newState };
 }
 
 /**
@@ -178,7 +178,7 @@ export function changeForm(newState) {
  * @return {object}          Formatted action for the reducer to handle
  */
 export function sendingRequest(sending) {
-	return { type: SENDING_REQUEST, sending };
+  return { type: SENDING_REQUEST, sending };
 }
 
 
@@ -187,24 +187,24 @@ export function sendingRequest(sending) {
  * @param message
  */
 function setErrorMessage(message) {
-	return (dispatch) => {
-		dispatch({ type: SET_ERROR_MESSAGE, message });
+  return (dispatch) => {
+    dispatch({ type: SET_ERROR_MESSAGE, message });
 
-		const form = document.querySelector('.form-page__form-wrapper');
-		if (form) {
-			form.classList.add('js-form__err-animation');
-			// Remove the animation class after the animation is finished, so it
-			// can play again on the next error
-			setTimeout(() => {
-				form.classList.remove('js-form__err-animation');
-			}, 150);
+    const form = document.querySelector('.form-page__form-wrapper');
+    if (form) {
+      form.classList.add('js-form__err-animation');
+      // Remove the animation class after the animation is finished, so it
+      // can play again on the next error
+      setTimeout(() => {
+        form.classList.remove('js-form__err-animation');
+      }, 150);
 
-			// Remove the error message after 3 seconds
-			setTimeout(() => {
-				dispatch({ type: SET_ERROR_MESSAGE, message: '' });
-			}, 3000);
-		}
-	}
+      // Remove the error message after 3 seconds
+      setTimeout(() => {
+        dispatch({ type: SET_ERROR_MESSAGE, message: '' });
+      }, 3000);
+    }
+  }
 }
 
 /**
@@ -212,8 +212,8 @@ function setErrorMessage(message) {
  * @param {string} location The route the user should be forwarded to
  */
 function forwardTo(location) {
-	console.log('forwardTo(' + location + ')');
-	browserHistory.push(location);
+  console.log('forwardTo(' + location + ')');
+  browserHistory.push(location);
 }
 
 
@@ -223,10 +223,10 @@ function forwardTo(location) {
  * @return {boolean}         True if there are empty elements, false if there aren't
  */
 function anyElementsEmpty(elements) {
-	for (let element in elements) {
-		if (!elements[element]) {
-			return true;
-		}
-	}
-	return false;
+  for (let element in elements) {
+    if (!elements[element]) {
+      return true;
+    }
+  }
+  return false;
 }
